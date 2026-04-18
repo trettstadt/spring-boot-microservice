e5d429892c78755ab9365e0c6039b55ceaa4eed0
Tobias Rettstadt <1839357+trettstadt@users.noreply.github.com>
2026-04-18 09:32:16 +0200

    upgrades for vulnerabilities

--- src/main/java/de/trettstadt/microservices/springbootmicroservice/SpringBootMicroserviceApplication.java
+++ src/main/java/de/trettstadt/microservices/springbootmicroservice/SpringBootMicroserviceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMicroserviceApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(SpringBootMicroserviceApplication.class, args);
     }
-
 }
